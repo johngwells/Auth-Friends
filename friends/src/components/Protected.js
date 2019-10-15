be03b781e29f799f27e3d8764879b03c145b8758
John Wells <molekewl2017@gmail.com>
2019-10-15 15:45:16 -0700

    add friendsList to Protected route

--- friends/src/components/Protected.js
+++ friends/src/components/Protected.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import FriendsList from './FriendsList';
 
 const Protected = () => {
   return (
     <div>
       Welcome USER!
+      <FriendsList />
     </div>
   )
 }
