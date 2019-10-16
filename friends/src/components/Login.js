e2358714644402526effb72508fe0f7666eafaf1
John Wells <molekewl2017@gmail.com>
2019-10-16 15:16:33 -0700

    css & placeholder for input

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -40,12 +40,14 @@ class Login extends React.Component {
             name='username'
             value={this.state.credentials.username}
             onChange={this.handleChange}
+            placeholder='Username'
           />
           <input 
             type='password'
             name='password'
             value={this.state.credentials.password}
             onChange={this.handleChange}
+            placeholder='Password'
           />
           <button>Log in</button>
         </form>
